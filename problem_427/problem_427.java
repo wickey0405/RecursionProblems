236fffa934e81b34148e50bb88f27f0f33d2d028
Admin of Recursion <admin@recursionist.io>
2021-04-05 16:52:34 +0900

    update ID427 revA

--- problem_427/problem_427.java
+++ problem_427/problem_427.java
@@ -12,60 +12,28 @@ class Node{
     }
 }
 
-class Deque{
+class Stack{
     public Node head;
-    public Node tail;
-
-    public Deque(){
+    
+    public Stack(){
         this.head = null;
-        this.tail = null;
     }
-    public Integer peekFront(){
+    public Integer peek(){
         if (this.head == null) return null;
         return this.head.data;
     }
-    public Integer peekBack(){
-        if (this.tail == null) return null;
-        return this.tail.data;
-    }
-    public void enqueueFront(int data){
-        if (this.head == null){
+    public void push(int data){
+        if (this.head == null) this.head = new Node(data);
+        else {
+            Node temp = this.head;
             this.head = new Node(data);
-            this.tail = this.head;
-        } else {
-            Node newNode = new Node(data);
-            this.head.prev = newNode;
-            newNode.next = this.head;
-            this.head = newNode;
+            this.head.next = temp;
         }
     }
-    public void enqueueBack(int data){
-        if (this.tail == null){
-            this.tail = new Node(data);
-            this.head = this.tail;
-        } else {
-            Node newNode = new Node(data);
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-    }
-    public Integer dequeueFront(){
+    public Integer pop(){
         if (this.head == null) return null;
-
         Node temp = this.head;
         this.head = this.head.next;
-        if(this.head != null) this.head.prev = null;
-        else this.tail = null;
-        return temp.data;
-    }
-    public Integer dequeueBack(){
-        if (this.tail == null) return null;
-
-        Node temp = this.tail;
-        this.tail = this.tail.prev;
-        if(this.tail != null) this.tail.next = null;
-        else this.head = null;
         return temp.data;
     }
 }
@@ -73,23 +41,25 @@ class Deque{
 class Solution{
     public static int[] stockSpan(int[] stocks){
         //ここから書きましょう
-        Deque deque = new Deque();
+        Stack stack = new Stack();
         int[] ans = new int[stocks.length];
-
         for (int i = 0; i < stocks.length; i++){
-            Node iterator = deque.tail;
             int count = 1;
-            while(iterator != null && iterator.data < stocks[i]){
-                iterator = iterator.prev;
-                count++;
+            if (stack.peek()==null || stocks[stack.peek()] > stocks[i]) stack.push(i);
+            else{
+                while(stack.peek() != null && stocks[stack.peek()] < stocks[i]){
+                count += ans[stack.pop()];
+                }
+                stack.push(i);
             }
-            deque.enqueueBack(stocks[i]);
+            
             ans[i] = count;
         }
         
         return ans;
     }
 }
+
 class Main{
     public static void main(String[] args){
         System.out.println(Arrays.toString(Solution.stockSpan(new int[]{1,2,3,4,5,6,4,5,1})));
